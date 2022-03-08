02b10f160d5b9f237a56cc87ec563c5b0bb829ed
heidi1105 <heidi1105@gmail.com>
2022-03-08 12:40:58 -0800

    Create functional_programming.js

---
+++ demo/w1d2/functional_programming.js
@@ -0,0 +1,19 @@
+// function intro
+
+// callback function
+
+// Object.freeze()
+
+// adding an item - spread
+
+// adding an item - concat
+
+// returning part of the array - slice
+
+// splice vs slice
+
+// sort 
+
+// map
+
+// filter
